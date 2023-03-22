f97fb20dc21b8fe04683217d8b56a7db0b9a3777
temtechie <templendukwu144@gmail.com>
2023-03-22 22:07:10 +0100

    implemented the Navbar section

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+// export { Default as Article } from './article/Article';
+// export { Default as  Brand } from './brand/Brand';
+// export { Default as CTA } from './cta/CTA';
+// export { Default as Feature } from './feature/Feature';
+// export { Default as Navbar } from './navbar/Navbar';
+
+
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/Brand';
+export { default as CTA } from './cta/CTA';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
+
---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+// export { Default as Blog } from './blog/Blog';
+// export { Default as  Features } from './features/Features';
+// export { Default as Footer } from './footer/Footer';
+// export { Default as Header } from './header/Header';
+// export { Default as Possibility } from './possibility/Possibility';
+// export { Default as WhatGPT3 } from './whatgpt3/WhatGPT3';
+
+export { default as Blog } from './blog/Blog';
+export { default as Features } from './features/Features';
+export { default as Footer } from './footer/Footer';
+export { default as Header } from './header/Header';
+export { default as Possibility } from './possibility/Possibility';
+export { default as WhatGPT3 } from './whatGPT3/WhatGPT3';

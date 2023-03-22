11fa861ca672e253e788c375c46c02ef8d3c1008
temtechie <templendukwu144@gmail.com>
2023-03-22 22:35:53 +0100

    implemented the header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,16 +7,16 @@ const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <p>Although any assistance for travel is appreciated, the idea of indulging in such luxury is unappealing. I have no inclination towards exercising any form of blessing. Instead, the joy of altering my routine and attaching myself to lively events brings me indulgence. Over the years, I have learned to allow myself to be more spontaneous and free-spirited.</p>
 
       <div className="gpt3__header-content__input">
-        <input type="email" placeholder="Your Email Address" />
+        <input type="email" placeholder="Enter your Email Address" />
         <button type="button">Get Started</button>
       </div>
 
       <div className="gpt3__header-content__people">
         <img src={people} />
-        <p>1,600 people requested access a visit in last 24 hours</p>
+        <p>3,200 people requested access a visit in last 24 hours</p>
       </div>
     </div>
 
